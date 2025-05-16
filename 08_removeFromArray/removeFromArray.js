594ad992dc29839ef96601c52ec1280fbd6acd69
Lotte <obdeijn.lotte@gmail.com>
2025-05-16 13:31:08 +0200

    removeFromArray test 1 passed

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    const myArray = arr;
+    const myValue = value;
+    const index = myArray.indexOf(myValue);
+    const x = myArray.splice(index, 1);
+
+    console.log(myArray);
+
+    return myArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
