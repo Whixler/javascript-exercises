adf1518e39be7ab59f217d4571d617d16f14e631
Lotte <obdeijn.lotte@gmail.com>
2025-05-16 15:13:34 +0200

    value array created

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,9 @@
-const removeFromArray = function(arr, value) {
+const removeFromArray = function(arr, ...value) {
     let myArray = arr;
     let myValue = value;
+
     while (true) {    
-        let index = myArray.indexOf(myValue);
+        let index = myArray.indexOf(myValue[0]);
         if(index >= 0) { 
             myArray.splice(index, 1);
         } else { break; }
