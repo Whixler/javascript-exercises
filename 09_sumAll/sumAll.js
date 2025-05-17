91ee99af35b29385804d3da13f1a2a6574766bd0
Lotte <obdeijn.lotte@gmail.com>
2025-05-17 08:45:48 +0200

    passed test 1 and 2

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+    console.log(numStart);
+    console.log(numEnd);
+    let total = 0;
+    if(numStart < 0 || numEnd < 0) {
+        return "ERROR";
+    } else {
 
+    for(let i = numStart; i <= numEnd; i++) {
+        total += i;
+    }}
+    return total;
 };
 
 // Do not edit below this line
