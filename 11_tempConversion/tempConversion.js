1a406f30ab8d63cf78bbf80c4231347cfee1a87a
Lotte <obdeijn.lotte@gmail.com>
2025-05-17 13:56:10 +0200

    passed all tempConverion tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) / 1.8;
+  return Math.round(celsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = temp * 1.8 + 32;
+  return Math.round(fahrenheit * 10) /10;
 };
 
 // Do not edit below this line
