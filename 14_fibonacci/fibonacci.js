700369d1e88799bfd6b3fafa188d7d42fb3d6bca
Lotte <obdeijn.lotte@gmail.com>
2025-05-21 14:23:17 +0200

    first 6 tests passed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+
+    if(num === 0){
+        return num1;
+    } else if (num === 1) {
+        return num2;
+    } else if (num > 1) {
+        for(let i = 2; i <= num; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }    
+        return num2
+    }
+
+
 
 };
 
