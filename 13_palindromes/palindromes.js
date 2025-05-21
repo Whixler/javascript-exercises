ea6bfc595c64286ab11b5169940c136f54d8d31f
Lotte <obdeijn.lotte@gmail.com>
2025-05-21 14:02:22 +0200

    passed all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,23 +1,9 @@
 const palindromes = function (palin) {
-    let clean = palin.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
-    console.log({clean});
-    const chars = clean.split("");
-
-    while (chars.length > 1){
-        let char1;
-        let char2;
-        let output = true;
-
-        if (output) {
-            output = char1 === char2;
-            char1 = chars.pop();
-            console.log(char1);
-            char2 = chars.shift();
-            console.log(char2);
-        }
-        return output;
-    }
-    
+    let clean = palin.replace(/[\W_]/g, '').toLowerCase();
+    const chars = clean.split("").reverse().join("");
+    console.log(clean);
+    console.log(chars);
+    return chars === clean;
 
 };
 
